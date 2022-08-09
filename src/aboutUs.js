dbf6ea379e1b5b4f65b9b0a007c12c832a0d86b3
pequito193 <vitorspequito@gmail.com>
2022-08-09 11:56:57 +0100

    Structure basic website layout

---
+++ src/aboutUs.js
@@ -0,0 +1,10 @@
+
+function createAboutUs() {
+
+    const container = document.querySelector('#container')
+
+    const header = document.createElement('h1')
+    header.textContent = 'About Us'
+
+    container.append(header)
+}
